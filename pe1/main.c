c4024f44e82293ec390c010626416870890c3968
Jonathon Roberts <jonrob1@hotmail.com>
2017-02-27 12:08:34 +0000

    Update

--- pe1/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int list35(int a);
-
-int main(int argc, char **argv)
-{
-     printf("%d\n", list35(10));
-       return 0;
-}
--- pe2/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int fib(int a);
-
-int main(int argc, char **argv)
-{
-     printf("%d\n", fib(10));
-       return 0;
-}
